d19b76454f03db58d9a7f6eb25ccc16888c1f42c
Amir Yunas <ayunas@amirs-mbp.attlocal.net>
2019-08-13 17:30:51 -0500

    added a reset button to reset the code display

--- keypad/src/components/Display.jsx
+++ keypad/src/components/Display.jsx
@@ -3,8 +3,10 @@ import { KeypadContext } from '../contexts/KeypadContext';
 
 function Display() {
     const { digit } = React.useContext(KeypadContext).displayDigit;
+    const {dispatch} = React.useContext(KeypadContext);
 
     return (
+        <>
         <div id="display">
             <div className="digit">{digit[0] && digit[0]}</div>
             <div className="digit">{digit[1] && digit[1]}</div>
@@ -13,6 +15,8 @@ function Display() {
             <div className="digit">{digit[4] && digit[4]}</div>
             <div className="digit">{digit[5] && digit[5]}</div>
         </div>
+        <button id="reset" onClick={() => dispatch({type: "RESET"})}>Reset</button>
+        </>
     )
 }
 
--- keypad/src/reducers/digitreducer.js
+++ keypad/src/reducers/digitreducer.js
@@ -10,7 +10,9 @@ export function digitreducer(state,action) {
             return state.digit.length <= 6 ? 
             {...state, digit: [...state.digit, action.digit] } 
             : state;
-           
+        case "RESET":
+            console.log('RESET case in reducer hit');
+            return {...state, digit : []};
         default: 
             return state;
     }
