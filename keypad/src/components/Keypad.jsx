47010414335686710d78b15412c3b74e188528c5
Amir Yunas <ayunas@gmail.com>
2019-08-13 17:43:22 -0500

    styled the keypad

--- keypad/src/components/Keypad.jsx
+++ keypad/src/components/Keypad.jsx
@@ -4,11 +4,11 @@ import {KeypadContext} from '../contexts/KeypadContext';
 
 function Keypad() {
     const digits = React.useContext(KeypadContext).digits;
-    
+
     return (
-        <>
+        <div id="keypad">
             {digits.map( digit => <Digit digit={digit}/> )}
-        </>
+        </div>
     )
 }
 
