babad8466758ae682b5a8637b1e63c981f8fd3e1
Amir Yunas <ayunas@gmail.com>
2019-08-13 18:11:22 -0500

    added click sound effects to the keypad using the UIFX library

--- keypad/src/components/Digit.jsx
+++ keypad/src/components/Digit.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import {digitreducer} from '../reducers/digitreducer';
 import {KeypadContext} from '../contexts/KeypadContext';
+import UIfx from 'uifx';
+import tickAudio from '../assets/clickSebastian.mp3';
+
+
 
 function Digit(props) {
     const context = React.useContext(KeypadContext);
+    const tick = new UIfx({
+        asset: tickAudio,
+        volume: 1.0
+    })
 
     return (
-        <button onClick={() => context.dispatch({ type:"DIGIT", digit: props.digit })}>{props.digit}</button>
+        <button onClick={() => {
+            tick.play();
+            context.dispatch({ type:"DIGIT", digit: props.digit })
+        }}>{props.digit}</button>
     )
 }
 
